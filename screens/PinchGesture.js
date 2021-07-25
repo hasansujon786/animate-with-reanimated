b84d38a3f1ad71d110d61504b3438c3b2b6cd41b
hasansujon786 <hasansujon786@gmail.com>
2021-07-26 00:13:12 +0600

    done: PinchGesture

--- screens/PinchGesture.js
+++ screens/PinchGesture.js
@@ -1,17 +1,68 @@
 import React from 'react'
-import {Image, StyleSheet, Text, View} from 'react-native'
+import { Dimensions, Image, StyleSheet, View } from 'react-native'
 import { PinchGestureHandler } from 'react-native-gesture-handler';
-import Animated from 'react-native-reanimated'
+import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 const imageUri =
   'https://images.unsplash.com/photo-1621569642780-4864752e847e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80';
 
+const { width, height } = Dimensions.get('window')
 const AnimatedImage = Animated.createAnimatedComponent(Image)
 
 const PinchGesture = () => {
+  const focalX = useSharedValue(0)
+  const focalY = useSharedValue(0)
+  const scale = useSharedValue(1)
+  const rPointerStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: focalX.value },
+        { translateY: focalY.value },
+      ]
+    }
+  })
+  const rImageStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: focalX.value },
+        { translateY: focalY.value },
+        { translateX: -width / 2 },
+        { translateY: -height / 2 },
+        { scale: scale.value },
+        { translateX: -focalX.value }, // TODO: figure out how transform really works
+        { translateY: -focalY.value },
+        { translateX: width / 2 },
+        { translateY: height / 2 },
+      ]
+    }
+  })
+
+  const handlePinchGesture = useAnimatedGestureHandler({
+    onActive(event) {
+      // active Object {
+      //   "eventName": "157onGestureHandlerEvent",
+      //   "focalX": 221.24969482421875,
+      //   "focalY": 446.89990234375,
+      //   "handlerTag": 4,
+      //   "numberOfPointers": 2,
+      //   "scale": 1.6686075097425825,
+      //   "state": 4,
+      //   "velocity": 0.0007547024070185815,
+      // }
+      scale.value = event.scale
+      focalX.value = event.focalX
+      focalY.value = event.focalY
+    },
+    onEnd() {
+      scale.value = withTiming(1)
+    }
+  })
   return (
     <View style={styles.screen}>
-      <PinchGestureHandler >
-        <AnimatedImage style={styles.image} source={{uri: imageUri}}/>
+      <PinchGestureHandler onGestureEvent={handlePinchGesture} >
+        <Animated.View style={{ flex: 1 }}>
+          <Animated.View style={[styles.pointer, rPointerStyles]} />
+          <AnimatedImage style={[styles.image, rImageStyles]} source={{ uri: imageUri }} />
+        </Animated.View>
       </PinchGestureHandler>
     </View>
   )
@@ -19,11 +70,22 @@ const PinchGesture = () => {
 
 export default PinchGesture
 
+const POINTER_SIZE = 30
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
   },
   image: {
-    flex: 1 
+    width,
+    height,
+  },
+  pointer: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 2,
+    width: POINTER_SIZE,
+    height: POINTER_SIZE,
+    borderRadius: POINTER_SIZE / 2,
+    backgroundColor: 'blue'
   }
 })
